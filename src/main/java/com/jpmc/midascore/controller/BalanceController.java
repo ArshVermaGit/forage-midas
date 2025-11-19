82d11fcde74f5b181f0991b00bf6a60c5f89f8b0
Arsh Verma <arshverma.dev@gmail.com>
2025-11-19 23:00:06 +0530

    Create BalanceController.java

---
+++ src/main/java/com/jpmc/midascore/controller/BalanceController.java
@@ -0,0 +1,26 @@
+package com.jpmc.midascore.controller;
+
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/balance")
+public class BalanceController {
+
+    private final UserRepository userRepository;
+
+    public BalanceController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public Balance getBalance(@RequestParam Long userId) {
+        UserRecord user = userRepository.findById(userId);
+        if (user != null) {
+            return new Balance(user.getBalance());
+        }
+        return new Balance(0.0f);
+    }
+}
